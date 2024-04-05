25b28684bf6fb2239fc54867d03dece71eb787f1
leens witts <leens@leenss-MacBook-Pro.local>
2024-04-05 22:29:50 +0300

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:instagram/screens/login_screen.dart';
 
 void main() {
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ScreenUtilInit(designSize: Size(375, 812), child: LoginScreen()),
+    );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,30 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 96.w,
+              height: 100.h,
+            ),
+            Center(
+              child: Image.asset('lib/assets/logo.jpg'),
+            ),
+            SizedBox(
+              height: 120.h,
+            ),
+            Container(
+              height: 44.h,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
